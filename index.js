cd975bffddd137d1e9f47d2465ed002140d35629
ishaikhfiroz <i.shaikhfiroz@gmail.com>
2023-06-16 10:42:58 +0530

    change 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2! 1506203 01')
+  res.send('CICD App V2! 1506203 02')
 })
 
 app.get('/status', (req, res) => {
